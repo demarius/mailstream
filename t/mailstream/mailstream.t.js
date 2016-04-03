fc0d5cd991ba3b4090a5df0a74aad2b518c0d947
Demarius Chrite <chrite.demarius@gmail.com>
2016-04-02 23:40:52 -0400

    Send some mail.

--- t/mailstream/mailstream.t.js
+++ t/mailstream/mailstream.t.js
@@ -1,1 +1,12 @@
 var mailer = require('../setup')
+
+console.log(mailer)
+
+mailer.sendMail({
+    from: 'test@testing.mailstream.com',
+    to: 'randomimapaddress@gmx.com',
+    subject: '',
+    html: ''
+}, function (err, info) {
+    console.log(info)
+})
