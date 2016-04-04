844f42050e0f2f4663a7eb949a451dc90674b820
Demarius Chrite <chrite.demarius@gmail.com>
2016-04-03 23:19:46 -0400

    Create a test mailstream.

--- t/mailstream/mailstream.t.js
+++ t/mailstream/mailstream.t.js
@@ -1,12 +1,20 @@
-var mailer = require('../setup')
+var mailer = require('../setup'),
+    MailStream = require('../../lib/mailstream')
 
-console.log(mailer)
-
-mailer.sendMail({
-    from: 'test@testing.mailstream.com',
-    to: 'randomimapaddress@gmx.com',
-    subject: '',
-    html: ''
-}, function (err, info) {
-    console.log(info)
+MailStream({
+    imap: {
+        user: 'randomimapaddress@gmx.com',
+        password: '0nSw1tching',
+        host: 'imap.gmx.com',
+        port: 993,
+        tls: true
+}, function (error, mailstream) {
+    mailer.sendMail({
+        from: 'test@testing.mailstream.com',
+        to: 'randomimapaddress@gmx.com',
+        subject: '',
+        html: ''
+    }, function (err, info) {
+        console.log(info)
+    })
 })
