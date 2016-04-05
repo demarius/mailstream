4b5945bfe1e052c67808f94440171fbb187bacf8
Demarius Chrite <chrite.demarius@gmail.com>
2016-04-05 18:01:30 -0400

    Fix typo.

--- t/mailstream/mailstream.t.js
+++ t/mailstream/mailstream.t.js
@@ -8,10 +8,12 @@ MailStream({
         host: 'imap.gmx.com',
         port: 993,
         tls: true
+    }
 }, function (error, mailstream) {
     mailstream.on('data', function (mail) {
         console.log(mail)
     })
+
     mailer.sendMail({
         from: 'test@testing.mailstream.com',
         to: 'randomimapaddress@gmx.com',
