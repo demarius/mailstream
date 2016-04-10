7af58b33710383f5d0e667033bb24327b3e93b97
Demarius Chrite <chrite.demarius@gmail.com>
2016-04-10 00:00:22 -0400

    Turn test into a Cadence function.

--- t/mailstream/mailstream.t.js
+++ t/mailstream/mailstream.t.js
@@ -1,31 +1,33 @@
-var mailer = require('../setup'),
-    MailStream = require('../../lib/mailstream')
+require('cadence')(function (async) {
+    var mailer = require('../setup'),
+        MailStream = require('../../lib/mailstream')
 
-MailStream({
-    imap: {
-        user: 'randomimapaddress@gmx.com',
-        password: '0nSw1tching',
-        host: 'imap.gmx.com',
-        port: 993,
-        tls: true
-    },
-    filter: function (mail) {
-        return mail.headers['mailstream-test-header'] ? true : false
-    }
-}, function (error, mailstream) {
-    mailstream.on('data', function (mail) {
-        console.log(mail)
-    })
-
-    mailer.sendMail({
-        from: 'test@testing.mailstream.com',
-        to: 'randomimapaddress@gmx.com',
-        subject: '',
-        html: '',
-        headers: {
-            'mailstream-test-header': 'mailstream-test'
+    MailStream({
+        imap: {
+            user: 'randomimapaddress@gmx.com',
+            password: '0nSw1tching',
+            host: 'imap.gmx.com',
+            port: 993,
+            tls: true
+        },
+        filter: function (mail) {
+            return mail.headers['mailstream-test-header'] ? true : false
         }
-    }, function (err, info) {
-        console.log(info)
+    }, function (error, mailstream) {
+        mailstream.on('data', function (mail) {
+            console.log(mail)
+        })
+
+        mailer.sendMail({
+            from: 'test@testing.mailstream.com',
+            to: 'randomimapaddress@gmx.com',
+            subject: '',
+            html: '',
+            headers: {
+                'mailstream-test-header': 'mailstream-test'
+            }
+        }, function (err, info) {
+            console.log(info)
+        })
     })
 })
